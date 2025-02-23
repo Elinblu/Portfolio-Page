b42885049623e250c80bcecb412724718e76923e
Elinblu <e_blu1797@hotmail.com>
2025-02-23 19:50:27 +0100

    New design

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let words = document.querySelectorAll(".word");
+  const words = document.querySelectorAll(".word");
   let index = 0;
 
   function showNextWord() {
     words[index].classList.remove("active");
-
     index = (index + 1) % words.length;
-
     words[index].classList.add("active");
   }
 
   setInterval(showNextWord, 3000);
+
+  const navbar = document.querySelector(".navbar");
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+
+  const navbarToggler = document.querySelector(".navbar-toggler");
+  const navbarMenu = document.querySelector("#navbarNav");
+
+  navbarToggler.addEventListener("click", function () {
+    navbarMenu.classList.toggle("show");
+
+    if (navbarMenu.classList.contains("show")) {
+      navbarMenu.style.display = "block";
+    } else {
+      navbarMenu.style.display = "none";
+    }
+  });
 });
